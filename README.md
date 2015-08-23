2f8dedf5f5784607948bbc5269025948c68ac12d
Ryan Siemens <ryanjsiemens@gmail.com>
2015-08-22 19:01:25 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mini URL
+#### URL minification service
+Turn this <https://www.youtube.com/watch?v=dQw4w9WgXcQ> ...
+Into this <http://localhost:5000/leb07>
+
+![mini url image](http://imgur.com/xu9bx4N)
---
+++ app/app.py
@@ -0,0 +1,70 @@
+import random
+import string
+
+from flask import Flask, request, redirect, render_template
+
+# Config
+DEBUG = True
+SECRET_KEY = 'this-needs-to-be-changed'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# globals
+address = 'http://localhost:5000'  # This is our address
+db = {}  # Replace this with a real database
+
+
+# Helper functions
+def url_check(url):
+    '''Attempt to ensure a valid url'''
+    if len(url.split('://')) != 2:
+        url = 'http://' + url
+    if url[-4:] != '.com':
+        url = url + '.com'
+    return url
+
+
+def build_rand(size):
+    '''Generate a random string 62**size'''
+    global db
+    base = string.digits + string.letters
+    while True:
+        rand = ''.join([random.choice(base) for x in xrange(size)])
+        if rand not in db.keys():
+            break
+    return rand
+
+
+# Controllers
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global db
+    if request.method == 'POST':
+        form = request.form['url']
+        hash = build_rand(5)
+        db[hash] = url_check(form)
+        return render_template('index.html', 
+                               result='http://localhost:5000/%s' % hash, 
+                               links=db.keys(),
+                               home=address)
+    return render_template('index.html',
+                           links=db.keys(),
+                           home=address)
+
+
+@app.route('/<hash>')
+def lookup(hash):
+    global db
+    try:
+        url = db[hash]
+        return redirect(url, code=302)
+    except KeyError:
+        return render_template('index.html',
+                               links=db.keys(),
+                               home=address)
+
+if __name__ == '__main__':
+    app.run()
