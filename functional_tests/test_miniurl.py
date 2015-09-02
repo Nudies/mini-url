735eb53cbc10b98ca0c9fdb2a79b326d451442bc
Ryan Siemens <ryanjsiemens@gmail.com>
2015-09-01 22:12:30 -0700

    Style and format changes

--- functional_tests/test_miniurl.py
+++ functional_tests/test_miniurl.py
@@ -2,36 +2,36 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class MiniUrl(unittest.TestCase):
-	
-	def setUp(self):
-		self.driver = webdriver.Firefox()
-		self.driver.get('http://localhost:5000')
 
-	def tearDown(self):
-		self.driver.close()
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://localhost:5000')
 
-	def test_can_submit_link(self):
-		# User sees a text input, puts in a url
-		self.assertIn('Mini URL', self.driver.title)
-		self.assertIn('Recent links', self.driver.page_source) 
-		input = self.driver.find_element_by_name('url')
-		input.send_keys('http://selenium-python.readthedocs.org')
-		input.send_keys(Keys.RETURN)
-	
-		# Shortened url appears below text input	
-		result = self.driver.find_element_by_id('result')
-		print dir(result)
+    def tearDown(self):
+        self.driver.close()
 
-		# Go to link
-		result.click()
-		print dir(result)
+    def test_can_submit_link(self):
+        # User sees a text input, puts in a url
+        self.assertIn('Mini URL', self.driver.title)
+        self.assertIn('See what people are minifying!', self.driver.page_source)
+        input = self.driver.find_element_by_name('url')
+        input.send_keys('http://selenium-python.readthedocs.org')
+        input.send_keys(Keys.RETURN)
 
-		# Ensure result is in recent links
-		recent = self.driver.find_elements_by_tag_name('li')[0]
-		self.assertEqual(result.text, recent.text)
+        # Shortened url appears below text input
+        result = self.driver.find_element_by_id('result')
+        print dir(result)
 
-		
-	
+        # Go to link
+        result.click()
+        print dir(result)
+
+        # Ensure result is in recent links
+        recent = self.driver.find_elements_by_tag_name('li')[1]
+        self.assertEqual(result.text, recent.text)
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
