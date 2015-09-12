b54d9eddb36e6ddfe082d9f51fdc13835b530956
Ryan Siemens <ryanjsiemens@gmail.com>
2015-09-11 20:45:08 -0700

    Complete the can_submit_link test

--- functional_tests/test_miniurl.py
+++ functional_tests/test_miniurl.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 class MiniUrl(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
         self.driver.get('http://localhost:5000')
 
     def tearDown(self):
@@ -17,16 +17,20 @@ class MiniUrl(unittest.TestCase):
         self.assertIn('Mini URL', self.driver.title)
         self.assertIn('See what people are minifying!', self.driver.page_source)
         input = self.driver.find_element_by_name('url')
-        input.send_keys('http://selenium-python.readthedocs.org')
+        test_url = 'http://selenium-python.readthedocs.org/'
+        input.send_keys(test_url)
         input.send_keys(Keys.RETURN)
 
         # Shortened url appears below text input
         result = self.driver.find_element_by_id('result')
-        print dir(result)
 
-        # Go to link
+        # Go to link and ensure it is what we want.
         result.click()
-        print dir(result)
+        windows = self.driver.window_handles
+        self.driver.switch_to_window(windows[1])
+        self.assertEqual(test_url, self.driver.current_url)
+        self.driver.close()
+        self.driver.switch_to_window(windows[0])
 
         # Ensure result is in recent links
         recent = self.driver.find_elements_by_tag_name('li')[1]
